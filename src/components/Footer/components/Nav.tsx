058367252da1b3d0da2a983e1a4caa2c1cce3bd3
xlong <horton.lin3@gmail.com>
2021-03-05 14:31:55 +0800

    替换部分地址

--- src/components/Footer/components/Nav.tsx
+++ src/components/Footer/components/Nav.tsx
@@ -4,13 +4,10 @@ import styled from 'styled-components'
 const Nav: React.FC = () => {
   return (
     <StyledNav>
-      <StyledLink href= {buyFBS} target="_blank">Get FBS</StyledLink>
       <StyledLink href= {buyFBC} target="_blank">Get FBC</StyledLink>
-      <StyledLink href="https://github.com/Basis-Cash" target="_blank">GitHub</StyledLink>
-      <StyledLink href="https://twitter.com/BasisCash" target="_blank">Twitter</StyledLink>
-      <StyledLink href="https://t.me/basiscash" target="_blank">Telegram</StyledLink>
-      <StyledLink href="https://discord.gg/UEZq3HF5Eu" target="_blank">Discord</StyledLink>
-      <StyledLink href="https://medium.com/basis-cash" target="_blank">Medium</StyledLink>
+      <StyledLink href= {buyFBS} target="_blank">Get FBS</StyledLink>
+      <StyledLink href="http://github.com/firebasiscash" target="_blank">GitHub</StyledLink>
+      <StyledLink href="https://t.me/hecofire" target="_blank">Telegram</StyledLink>
       <StyledLink href="https://www.dropbox.com/s/ed5vxvaple5e740/REP-Basis-Cash-06_11_2020.pdf?dl=0" target="_blank">Audit</StyledLink>
     </StyledNav>
   )
